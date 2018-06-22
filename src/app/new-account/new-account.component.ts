dc107f0693b6958503d7a9f148289a50242c7f26
robertHer <totolero769@gmail.com>
2018-06-22 15:41:02 -0700

    injecting login service into component

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -5,18 +5,25 @@ import {LogginService} from '../login.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LogginService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private logginService: LogginService){
+
+  } //binding to property
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
+    this.logginService.logStatusChange
+
     const service = new LogginService();
     service.logStatusChange(accountStatus);
-    // console.log('A server status changed, new status: ' + accountStatus);
+    console.log('A server status changed, new status: ' + accountStatus);
   }
 }
